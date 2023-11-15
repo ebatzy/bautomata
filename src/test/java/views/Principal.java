dd189f46b8ce0bb21adf48f8cde65b371a7c7dee
Roberto B <roesbero@gmail.com>
2023-11-15 15:31:19 -0600

    Se agrego mensaje de error o éxito a la interfaz

--- src/test/java/views/Principal.java
+++ src/test/java/views/Principal.java
@@ -6,12 +6,16 @@ import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridBagConstraints;
-import java.awt.GridLayout;
+import java.awt.GridBagLayout;
 import java.awt.Image;
+import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
 import java.io.IOException;
 
+import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
@@ -21,6 +25,8 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
 import javax.swing.SwingUtilities;
+import javax.swing.border.Border;
+import javax.swing.border.LineBorder;
 
 import config.Preferencias;
 import controllers.Login;
@@ -35,22 +41,37 @@ public class Principal {
 	static ImageIcon iconoFirefox = new ImageIcon("src/test/resources/img/Firefox.png");
 	static ImageIcon iconoEdge = new ImageIcon("src/test/resources/img/Edge.png");
 	static ImageIcon iconoSafari = new ImageIcon("src/test/resources/img/Safari.png");
+	static ImageIcon iconoFallo = new ImageIcon("src/test/resources/img/Fallo.png");
+	static ImageIcon iconoCorrecto = new ImageIcon("src/test/resources/img/Correcto.png");
+
+	static String[] arregloResultado;
+	static ImageIcon iconoResultado;
+	static Color colorResultado;
+	static Boolean mostrarResultado = false;
+
+	private static PropertyChangeSupport escucha;
 
 	public Principal() {
 
 	}
 
 	public static void main(String[] args) {
+		escucha = new PropertyChangeSupport(new Principal());
 
 		Color colorFondo = new Color(158, 218, 255);
 
 		SwingUtilities.invokeLater(() -> {
+
+			Principal obj = new Principal();
+
 			JFrame frame = new JFrame("Automatizaciones Transversales");
 			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			frame.setLayout(new GridLayout(1, 2, 3, 4));
+			frame.setLayout(new GridBagLayout());
+			GridBagConstraints c = new GridBagConstraints();
 
 			JPanel panelNivel = new JPanel(new BorderLayout());
 			panelNivel.setBackground(colorFondo);
+			panelNivel.setPreferredSize(new Dimension(150, 270));
 			JLabel tituloNivel = new JLabel("Nivel", SwingConstants.CENTER);
 			tituloNivel.setPreferredSize(new Dimension(300, 25));
 			tituloNivel.setFont(new Font("Arial", Font.BOLD, 15));
@@ -58,8 +79,9 @@ public class Principal {
 
 			JPanel panelBrowser = new JPanel(new BorderLayout());
 			panelBrowser.setBackground(colorFondo);
+			panelBrowser.setPreferredSize(new Dimension(150, 270));
 			JLabel tituloBrowser = new JLabel("Navegador", SwingConstants.CENTER);
-			tituloBrowser.setPreferredSize(new Dimension(300, 25));
+			tituloBrowser.setPreferredSize(new Dimension(150, 25));
 			tituloBrowser.setFont(new Font("Arial", Font.BOLD, 15));
 			panelBrowser.add(tituloBrowser, BorderLayout.NORTH);
 
@@ -75,6 +97,7 @@ public class Principal {
 
 			JPanel panelIzquierdo = new JPanel(new BorderLayout());
 			panelIzquierdo.setBackground(new Color(158, 218, 255));
+			panelIzquierdo.setPreferredSize(new Dimension(150, 270));
 			JLabel tituloIzquierdo = new JLabel("QA's", SwingConstants.CENTER);
 			tituloIzquierdo.setPreferredSize(new Dimension(300, 25));
 			tituloIzquierdo.setFont(new Font("Arial", Font.BOLD, 15));
@@ -87,6 +110,7 @@ public class Principal {
 
 			JPanel panelDerecho = new JPanel(new BorderLayout());
 			panelDerecho.setBackground(new Color(158, 218, 255));
+			panelDerecho.setPreferredSize(new Dimension(200, 270));
 			JLabel tituloDerecho = new JLabel("Tests", SwingConstants.CENTER);
 			tituloDerecho.setPreferredSize(new Dimension(300, 25));
 			tituloDerecho.setFont(new Font("Arial", Font.BOLD, 15));
@@ -100,7 +124,6 @@ public class Principal {
 			JButton botonNivel1 = new JButton("Nivel 1");
 			botonNivel1.setEnabled(true);
 			botonNivel1.setAlignmentX(Component.CENTER_ALIGNMENT);
-			GridBagConstraints c = new GridBagConstraints();
 			c.insets.set(10, 10, 10, 10);
 			botonesNiveles.add(botonNivel1);
 			botonNivel1.addActionListener(e -> {
@@ -158,6 +181,8 @@ public class Principal {
 			botonNivel3.addActionListener(new ActionListener() {
 				@Override
 				public void actionPerformed(ActionEvent e) {
+					String[] arreglo = {  };
+					resultado(arreglo);
 					for (Component btnsLevels : botonesNiveles.getComponents()) {
 						if (btnsLevels instanceof JButton) {
 							btnsLevels.setEnabled(false);
@@ -348,6 +373,7 @@ public class Principal {
 				botonSafari.setBackground(colorFondo);
 			});
 			botonSafari.addActionListener(new ActionListener() {
+
 				@Override
 				public void actionPerformed(ActionEvent e) {
 					for (Component btnsBrowsers : botonesBrowsers.getComponents()) {
@@ -363,12 +389,59 @@ public class Principal {
 				}
 			});
 
-			frame.add(panelNivel);
-			frame.add(panelBrowser);
-			frame.add(panelIzquierdo);
-			frame.add(panelDerecho);
+			JPanel panelMensaje = new JPanel();
+			panelMensaje.setLayout(new BoxLayout(panelMensaje, BoxLayout.Y_AXIS));
+
+			obj.addPropertyChangeListener(evt -> {
+
+				panelMensaje.setBackground(colorResultado);
+				JLabel label = new JLabel(iconoRedimensionado2(iconoResultado));
+				panelMensaje.setBorder(new LineBorder(Color.black));
+				label.setHorizontalAlignment(JLabel.CENTER);
+				panelMensaje.add(label);
+				panelMensaje.setVisible(mostrarResultado);
+				System.out.println("LLego aqui");
+
+				String texto = "";
+
+				for (String elemento : arregloResultado) {
+					texto += elemento + "<br>";
+				}
+
+				label.setText("<html>" + texto + "</html>");
+
+				Border border = BorderFactory.createEmptyBorder(15, 5, 15, 25);
+				label.setBorder(border);
+				new LineBorder(Color.RED, 4);
+
+				label.setAlignmentX(JLabel.CENTER_ALIGNMENT);
+				label.setAlignmentY(JLabel.CENTER_ALIGNMENT);
+
+				Dimension size = label.getPreferredSize();
+				label.setPreferredSize(size);
+				label.setFont(new Font("Serif", Font.BOLD, 12));
+				label.setForeground(Color.white);
+				frame.pack();
+
+			});
+
+			c.insets = new Insets(5, 2, 2, 2);
+
+			c.gridx = 0;
+			c.gridy = 0;
+			frame.add(panelNivel, c);
+			c.gridx = 1;
+			frame.add(panelBrowser, c);
+			c.gridx = 2;
+			frame.add(panelIzquierdo, c);
+			c.gridx = 3;
+			frame.add(panelDerecho, c);
+			c.gridy = 1;
+			c.gridwidth = 4;
+			c.gridx = 0;
+			frame.add(panelMensaje, c);
+			frame.getContentPane().setBackground(new Color(199, 238, 255));
 			frame.pack();
-			frame.setSize(400, 300);
 			frame.setVisible(true);
 		});
 	}
@@ -379,36 +452,36 @@ public class Principal {
 		String temp = "";
 
 		switch (valor) {
-		case 1:
-			temp = "QA01";
-			break;
-		case 2:
-			temp = "QA02";
-			break;
-		case 3:
-			temp = "QA03";
-			break;
-		case 4:
-			temp = "QA04";
-			break;
-		case 5:
-			temp = "QA05";
-			break;
-		case 6:
-			temp = "QA06";
-			break;
-		case 7:
-			temp = "QA07";
-			break;
-		case 8:
-			temp = "QA08";
-			break;
-		case 9:
-			temp = "Produccion";
-			break;
-		default:
-			temp = "Produccion";
-			break;
+			case 1:
+				temp = "QA01";
+				break;
+			case 2:
+				temp = "QA02";
+				break;
+			case 3:
+				temp = "QA03";
+				break;
+			case 4:
+				temp = "QA04";
+				break;
+			case 5:
+				temp = "QA05";
+				break;
+			case 6:
+				temp = "QA06";
+				break;
+			case 7:
+				temp = "QA07";
+				break;
+			case 8:
+				temp = "QA08";
+				break;
+			case 9:
+				temp = "Produccion";
+				break;
+			default:
+				temp = "Produccion";
+				break;
 		}
 
 		preferencias.valorAtributo("paginaWeb", Bi_helper.obtenerDato(temp, "url", AMBIENTE_JSON));
@@ -420,4 +493,33 @@ public class Principal {
 		ImageIcon iconoRedimensionado = new ImageIcon(imagenRedimensionada);
 		return iconoRedimensionado;
 	}
+
+	private static Icon iconoRedimensionado2(ImageIcon icono) {
+		Image imagenRedimensionada = icono.getImage().getScaledInstance(36, 36, Image.SCALE_SMOOTH);
+		ImageIcon iconoRedimensionado = new ImageIcon(imagenRedimensionada);
+		return iconoRedimensionado;
+	}
+
+	public static void resultado(String[] errores) {
+		String[] old = arregloResultado;
+		mostrarResultado = true;
+		if (errores.length == 0) {
+			String arreglo[] = { "Test ejecutado exitosamente" };
+			arregloResultado = arreglo;
+			colorResultado = Color.green;
+			iconoResultado = (ImageIcon) iconoRedimensionado2(iconoCorrecto);
+		} else {
+			arregloResultado = errores;
+			System.out.println("Prueba");
+			colorResultado = Color.red;
+			iconoResultado = (ImageIcon) iconoRedimensionado2(iconoFallo);
+
+		}
+		escucha.firePropertyChange("arregloResultado", old, errores);
+	}
+
+	public void addPropertyChangeListener(PropertyChangeListener listener) {
+		escucha.addPropertyChangeListener(listener);
+	}
+
 }
